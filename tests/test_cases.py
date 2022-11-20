8b66aa79ef4ee381277d9dcb0bca9eaf46ffa494
Gregory Edison <gregory.edison1993@gmail.com>
2022-11-20 11:09:40 +0100

    test: additional test case

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -26,7 +26,7 @@ async def test(starknet):
     contract = await starknet.deploy(source="contracts/simulator/simulator.cairo")
     LOGGER.info(f"> Deployed simulator.cairo.")
 
-    ### Test case 1 ###
+    # ### Test case 1 ###
 
     (mechs, instructions_length, instructions, inputs, outputs, types) = import_json(
         "./tests/test-cases/test1.json"
@@ -147,10 +147,53 @@ async def test(starknet):
     ), f'static cost error, expected 7000, got {frames["static cost"]}'
     assert (
         frames["delivered"] == 5
-    ), f'delivered error, expected 2, got {frames["delivered"]}'
+    ), f'delivered error, expected 5, got {frames["delivered"]}'
     assert (
         frames["average latency"] / 1000000 == 20
     ), f'average latency error, expected 20, got {frames["average latency"]/1000000}'
     assert (
         frames["average dynamic cost"] / 1000000 == 3780.8
     ), f'average dynamic cost error, expected 3780.8, got {frames["average dynamic cost"]/1000000}'
+
+    ### Test case 4 ###
+
+    (mechs, instructions_length, instructions, inputs, outputs, types) = import_json(
+        "./tests/test-cases/test4.json"
+    )
+
+    # # Loop the baby
+    ret = await contract.simulator(
+        mechs, instructions_length, instructions, inputs, outputs, types
+    ).call()
+
+    events = ret.main_call_events
+
+    LOGGER.info(
+        f"> Simulation of 100 frames took execution_resources = {ret.call_info.execution_resources}"
+    )
+
+    frames = {
+        "solver": events[0].solver,
+        "instructions length per mech": events[0].instructions_sets,
+        "instructions": events[0].instructions,
+        "operators input": events[0].operators_inputs,
+        "operators ouput": events[0].operators_outputs,
+        "operators type": events[0].operators_type,
+        "static cost": events[0].static_cost,
+        "delivered": events[-1].delivered,
+        "average latency": events[-1].latency,
+        "average dynamic cost": events[-1].dynamic_cost,
+    }
+
+    assert (
+        frames["static cost"] == 5500
+    ), f'static cost error, expected 5500, got {frames["static cost"]}'
+    assert (
+        frames["delivered"] == 7
+    ), f'delivered error, expected 7, got {frames["delivered"]}'
+    assert (
+        frames["average latency"] / 1000000 == 13.142857
+    ), f'average latency error, expected 13.142857, got {frames["average latency"]/1000000}'
+    assert (
+        frames["average dynamic cost"] / 1000000 == 2860.857142
+    ), f'average dynamic cost error, expected 2860.857142, got {frames["average dynamic cost"]/1000000}'
